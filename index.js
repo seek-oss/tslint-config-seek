d911623f9fee96fb5b243935005ca5cf54639e34
Ryan Cumming <etaoins@gmail.com>
2019-08-13 19:40:10 +1000

    feat(rules): Add stricter async/await rules
    This adds two rules that have been used in Indirect Apply for a while:
    
    - `await-promise` triggers when awaiting on a non-promise value. While
      this is defined to just return the value it's almost certainly an
      error.
    
    - `no-async-without-await` triggers when a function is `async` without
      either an `await` or a return value. These functions can typically
      either be synchronous or the programmer forgot an `await`.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = {
   extends: ['tslint:latest', 'tslint-config-prettier'],
   rules: {
+    'await-promise': true,
     'interface-name': [true, 'never-prefix'],
     'member-access': false,
+    'no-async-without-await': true,
     'no-floating-promises': true,
     'no-implicit-dependencies': [true, 'dev'],
     'no-submodule-imports': false,
